2db80081a9d9ce307f112e27ef3cb21399ab1a7b
Thales Gomes Targino <thalestargino@gmail.com>
2023-08-04 13:30:09 -0300

    feat: adicionando arquivo

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,8 @@
 function Home() {
-	return <h1>Home</h1>;
+  return <>
+    <h1>Projeto do curso.dev com o Filipe Deschamps</h1>
+    <p>Obrigado por compartilhar</p>
+  </>
 }
 
 export default Home;
